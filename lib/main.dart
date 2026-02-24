093e2642a59570fb9df8253972e1f732aba22c21
dexter <dextervale87@gmail.com>
2026-02-24 19:41:30 +0800

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -14,20 +14,19 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     const title = 'Basic List';
     return MaterialApp(
-      //title: title,
-      home: HorizontalList()
-      //Scaffold(
-        //appBar: AppBar(title: const Text(title),),
-       /* body: ListView(
+    title: title,
+      home: Scaffold(
+        appBar: AppBar(title: const Text(title),),
+        body: ListView(
           children: const <Widget>[
             ListTile(leading: Icon(Icons.map), title: Text('Map'),),
             ListTile(leading: Icon(Icons.photo_album), title: Text('Album'),),
             ListTile(leading: Icon(Icons.phone), title: Text('Phone'),)
           ],
         ),
-         */
+         
 
-     // ),
+      ),
      
     );
   }
